1d6175f6aea1dcedbc12ac4ae97c595b530e690d
vulus98 <bozic.vukasin@gmail.com>
2026-02-11 18:42:04 +0100

    script works

--- run_slurm.sh
+++ run_slurm.sh
@@ -1,19 +1,20 @@
 #!/usr/bin/env bash
 
-#SBATCH --job-name=pano-infinigen-array1
-#SBATCH --array=1-1000
-#SBATCH -n 12
+#SBATCH --job-name=pano-infinigen-array
+#SBATCH --array=1-1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=12
+#SBATCH --mem-per-cpu=2G
 #SBATCH --time=4:00:00
-#SBATCH --mem-per-cpu=2000
-#SBATCH --gpus=1
 #SBATCH --tmp=208000
+#SBATCH -o logs/%A_%a.out
+#SBATCH --gpus=rtx_4090:1
 
 echo "$(date) start ${SLURM_JOB_ID}"
 
 module load eth_proxy
 
-name=pano-infinigen-array1
-# id=${SLURM_JOB_ID} #03790512 #02691156 #03790512 #02958343
+name=pano-infinigen-array
 num_scenes=1
 num_concurrent=32
 folder_name="${SLURM_JOB_ID}_${SLURM_ARRAY_TASK_ID}"
@@ -23,7 +24,7 @@ python -m infinigen.datagen.manage_jobs --output_folder outputs/outdoor/$folder_
     --configs simple.gin \
     --pipeline_configs local_256GB.gin monocular.gin blender_gt.gin \
     --pipeline_overrides LocalScheduleHandler.use_gpu=True \
-    --wandb_mode online 
+    --wandb_mode disabled 
 
 # Generate indoor
 # python -m infinigen.datagen.manage_jobs --output_folder outputs/indoor/$folder_name --num_scenes $num_scenes --pipeline_configs local_256GB.gin monocular.gin blender_gt.gin indoor_background_configs.gin \
