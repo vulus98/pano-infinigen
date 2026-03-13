5818c40297a4d6d049431451c59ebb34d2a7143f
vulus98 <bozic.vukasin@gmail.com>
2026-03-13 02:57:47 +0100

    deleted obsolete files

--- run_local.sh
+++ run_local.sh
@@ -1,15 +1,13 @@
-# Generate outdoor 
-python -m infinigen.datagen.manage_jobs --output_folder outputs/outdoor --num_scenes 1 \
---configs simple.gin --pipeline_configs local_256GB.gin monocular.gin blender_gt.gin --pipeline_overrides LocalScheduleHandler.use_gpu=True
+# Indoor
+# python -m infinigen.datagen.manage_jobs --output_folder outputs/indoor --num_scenes 2 \
+# --pipeline_configs local_256GB.gin monocular.gin blender_gt.gin indoor_background_configs.gin \
+# --configs singleroom.gin fast_solve.gin \
+# --pipeline_overrides iterate_scene_tasks.n_camera_rigs=6 get_cmd.driver_script='infinigen_examples.generate_indoors' manage_datagen_jobs.num_concurrent=16 LocalScheduleHandler.use_gpu=True \
+# --overrides compose_indoors.restrict_single_supported_roomtype=True camera.camera_pose_proposal.pitch=90 camera.camera_pose_proposal.roll=0 compose_indoors.lights_off_chance=0.0
 
-# python -m infinigen.datagen.manage_jobs --output_folder outputs/my_outdoor_dataset --num_scenes 1 \
-# --configs simple.gin fast_solve.gin --overwrite --pipeline_configs local_256GB.gin monocular.gin blender_gt.gin cuda_terrain.gin --cleanup big_files --warmup_sec 1200 \
-# --pipeline_overrides LocalScheduleHandler.use_gpu=True \
-# --config no_particles no_creatures
-
-# Generate indoor 
-# python -m infinigen.datagen.manage_jobs --output_folder outputs/my_indoor_dataset4 --num_scenes 1 --pipeline_configs local_256GB.gin monocular.gin blender_gt.gin indoor_background_configs.gin \
-#     --configs singleroom.gin fast_solve.gin \
-#     --pipeline_overrides get_cmd.driver_script='infinigen_examples.generate_indoors' manage_datagen_jobs.num_concurrent=16 \
-#     --overrides compose_indoors.restrict_single_supported_roomtype=True #\
-    # --wandb_mode online
+# Outdoor
+python -m infinigen.datagen.manage_jobs --output_folder outputs/outdoor --num_scenes 2 \
+    --configs simple.gin \
+    --pipeline_configs local_256GB.gin monocular.gin blender_gt.gin \
+    --pipeline_overrides manage_datagen_jobs.num_concurrent=16 LocalScheduleHandler.use_gpu=True \
+    --overrides camera.camera_pose_proposal.pitch=90 camera.camera_pose_proposal.roll=0
